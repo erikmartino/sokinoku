367a7f248751a53230bbe977b7acbc8d8163df6b
Erik Martino <martino@chainalysis.com>
2020-03-25 09:01:50 +0100

    work

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import { GameBoard } from './GameBoard';
 
+function keyPresses(event: React.KeyboardEvent) {
+    console.log(event);
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div onKeyDown={keyPresses} className="App">
         <GameBoard />
-      </header>
     </div>
   );
 }
--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -1,51 +1,212 @@
 import React from 'react';
 import './game.css';
 
+
+function parseBoard(board: string) {
+    let rows: string[] = board.split(/\r?\n/);
+}
+
+class Piece {
+    constructor(x: number, y: number, piece: string) {
+        this.x = x;
+        this.y = y;
+        this.piece = piece;
+    }
+
+    x: number;
+    y: number;
+    piece: string;
+}
+
 interface Props {
 
 }
 
-interface State {
 
+enum DIRECTION {
+    RIGHT, UP, LEFT, DOWN
+}
+
+class GameState implements State {
+    constructor() {
+        this.width = 8;
+        this.height = 9;
+        this.board = //
+            `########
+#####  #
+#   $  #
+#  .#  #
+## ## ##
+#      #
+# @#   #
+#  #####
+########`;
+        this.pieces = [];
+        this.extractPieces('.');
+        this.extractPieces('@');
+        this.extractPieces('$');
+    }
+
+    extractPieces(p: string) {
+        let pieces = this.pieces;
+        let board = this.board;
+
+        let index: number = 0;
+        for (; ;) {
+            index = board.indexOf(p, index + 1);
+            if (index < 0) {
+                break;
+            }
+            let x = index % (this.width + 1);
+            let y = (index - x) / (this.width + 1);
+            console.log(x + "," + y + " " + p)
+
+            pieces.push(new Piece(x, y, p));
+        }
+
+        board = board.replace(p, ' ');
+        this.board = board;
+        this.pieces = pieces;
+    }
+
+    width: number;
+    height: number;
+    board: string;
+    pieces: Piece[];
+
+    tiles(x: number, y: number): string {
+        if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
+            return "";
+        }
+
+        return this.board[x + (this.width + 1) * y];
+    }
+
+    move(dir: DIRECTION): void {
+        this.pieces.filter(p => p.piece === '@').forEach(p => {
+            let x = p.x;
+            let y = p.y;
+
+            switch (dir) {
+                case DIRECTION.RIGHT:
+                    x++;
+                    break;
+                case DIRECTION.UP:
+                    y--;
+                    break;
+                case DIRECTION.LEFT:
+                    x--;
+                    break;
+                case DIRECTION.DOWN:
+                    y++;
+                    break;
+            }
+
+            if (this.tiles(x, y) !== ' ') {
+                return;
+            }
+
+            p.x = x;
+            p.y = y;
+
+
+            console.log('move ' + p.x + ',' + p.y);
+        });
+    }
+}
+
+
+const CLASSNAMES: { [key: string]: string; } = {
+    "#": "wall",
+    " ": "floor",
+    "@": "player",
+    "$": "crate",
+    ".": "endpoint",
+};
+
+interface State {
+    width: number;
+    height: number;
+    board: string;
+    pieces: Piece[];
+
+    move(dir: DIRECTION): void;
+
+    tiles(x: number, y: number): string;
 }
 
 export class GameBoard extends React.Component<Props, State> {
+    private keyDown: (e: KeyboardEvent) => void;
+
     constructor(props: Props) {
         super(props);
-        this.state = {}
+        this.state = new GameState();
+        this.keyDown = (e: KeyboardEvent) => this.keyPresses(e);
     }
 
-    column(j: number): React.ReactElement[] {
-        let column = [];
-        for (let i = 0; i < 10; i++) {
-            let key = `(${i},${j})`;
-            column.push(<div className="block" key={key} />);
+    keyPresses(event: KeyboardEvent) {
+        switch (event.code) {
+            case "ArrowRight":
+                this.state.move(DIRECTION.RIGHT);
+                break;
+            case "ArrowUp":
+                this.state.move(DIRECTION.UP);
+                break;
+            case "ArrowLeft":
+                this.state.move(DIRECTION.LEFT);
+                break;
+            case "ArrowDown":
+                this.state.move(DIRECTION.LEFT);
+                break;
+            default:
+                console.log(event.code);
         }
-        return column;
+        this.setState(this.state);
     }
 
-    arow(): React.ReactElement[] {
-        let rows = [];
-        for (let j = 0; j < 10; j++) {
-            rows.push(<div key={j}>{this.column(j)}</div>);
+    shouldComponentUpdate() {
+        return true;
+    }
+
+    componentDidMount() {
+        document.body.addEventListener('keydown', this.keyDown);
+    }
+
+    componentWillUnmount(): void {
+        document.body.removeEventListener('keydown', this.keyDown);
+    }
+
+    renderedTiles(): React.ReactElement[] {
+        let cells: React.ReactElement[] = []
+        for (let i = 0; i < this.state.height; i++) {
+            for (let j = 0; j < this.state.width; j++) {
+                let p = this.state.tiles(j, i);
+                let classNames = CLASSNAMES[p];
+                cells.push(<div key={i + ',' + j} className={classNames}/>);
+            }
         }
-        return rows;
+        return cells;
+    }
+
+    renderedPieces(): React.ReactElement[] {
+        let pieces: React.ReactElement[] = []
+        this.state.pieces.forEach(p => {
+            let classNames = CLASSNAMES[p.piece];
+            let style = {left: 64 * p.x + 'px', top: 64 * p.y + 'px'};
+            pieces.push(<div key={'piece' + p.x + ',' + p.y} className={classNames} style={style}/>)
+        });
+        console.log(pieces);
+        return pieces;
     }
 
     render(): React.ReactElement {
-        let rows = this.arow();
-        console.log("msg: " + rows);
-        return <div className="game_board">
-            {rows}
+        return <div className='game'>
+            <div key='game_board' className="game_board">
+                {this.renderedTiles()}
+            </div>
+            <div key='pieces' className='game_pieces'>
+                {this.renderedPieces()}
+            </div>
         </div>;
     }
 }
-
-// for (let i = 0; i < 10; i++) {
-//     return <tr>
-//         {() => {
-//             for (let j = 0; j < 10; j++) {
-//                 return <td>{i + j}</td>;
-//             }
-//         }}
-//     </tr>
